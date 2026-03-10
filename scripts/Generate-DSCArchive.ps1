5c53706d3b276a7f8726199a50f0a5d99564e43e
Yvan Duhamel <yvandev@outlook.fr>
2026-03-10 11:22:00 +0100

    add tasks

--- scripts/Generate-DSCArchive.ps1
+++ scripts/Generate-DSCArchive.ps1
@@ -4,13 +4,13 @@
 # Get-Command -Verb Publish -Noun AzVM* -Module Az.Compute
 
 param(
-    [string] $vmName = "*",
-    [string] $dscFolderPath = ".\dsc"
+    [string] $dscConfigFile = "*",
+    [string] $dscFolderPath
 )
 
 if (Test-Path $dscFolderPath) {
-    Write-Host "Generating DSC archives in folder '$dscFolderPath' for VMs '$vmName'" -ForegroundColor Cyan
-    $dscSourceFilePaths = Get-ChildItem $dscFolderPath -File -Filter "Configure$vmName*.ps1"
+    Write-Host "Generating DSC archives in folder '$dscFolderPath' for '$dscConfigFile'" -ForegroundColor Cyan
+    $dscSourceFilePaths = Get-ChildItem $dscFolderPath -File -Filter "$dscConfigFile*.ps1"
     foreach ($dscSourceFilePath in $dscSourceFilePaths) {
         $dscArchiveFilePath = "$($dscSourceFilePath.DirectoryName)\$($dscSourceFilePath.BaseName).zip"
         Publish-AzVMDscConfiguration -ConfigurationPath "$dscFolderPath\$($dscSourceFilePath.Name)" -OutputArchivePath $dscArchiveFilePath -Force -Verbose
