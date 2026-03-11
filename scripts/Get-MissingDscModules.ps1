680f584e308fb7a01a187d274846926ed09d6653
Yvan Duhamel <yvandev@outlook.fr>
2026-03-11 13:29:29 +0100

    rename script to Get-MissingDscModules

--- scripts/Get-MissingDscModules.ps1
+++ scripts/Get-MissingDscModules.ps1
@@ -9,8 +9,9 @@ $dscFilePathResolved = Resolve-Path -Path $dscFilePath -ErrorAction SilentlyCont
 if (Test-Path -PathType Leaf -Path $dscFilePathResolved) {
     Write-Host "Getting module dependencies for DSC file '$dscFilePathResolved'" -ForegroundColor Cyan
     $parseResult = [Microsoft.WindowsAzure.Commands.Common.Extensions.DSC.Publish.ConfigurationParsingHelper]::ParseConfiguration($dscFilePathResolved)
-    return $parseResult.RequiredModules
+    # return $parseResult.RequiredModules
+    return $parseResult.Errors | Where-Object ErrorId -eq "ModuleNotFoundDuringParse" | Select-Object -Property Extent
 }
-else {  
+else {
     Write-Host "file '$dscFilePath' not found" -ForegroundColor Red
 }
