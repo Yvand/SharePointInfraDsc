95f24b6c02176239602ad309d03ee870f0e5fb0f
Yvan Duhamel <yvandev@outlook.fr>
2026-03-12 16:32:29 +0100

    update scripts

--- scripts/Get-MissingDscModules.ps1
+++ scripts/Get-MissingDscModules.ps1
@@ -6,25 +6,24 @@ param(
 
 #Import-Module Az.Compute
 $dscFilePathResolved = Resolve-Path -Path $dscFilePath -ErrorAction SilentlyContinue
-if (Test-Path -PathType Leaf -Path $dscFilePathResolved) {
-    Write-Host "Getting module dependencies for DSC file '$dscFilePathResolved'" -ForegroundColor Cyan
-    $parseResult = [Microsoft.WindowsAzure.Commands.Common.Extensions.DSC.Publish.ConfigurationParsingHelper]::ParseConfiguration($dscFilePathResolved)
-    
-    $missingModules = @()
-    foreach ($parseErr in $parseResult.Errors | Where-Object ErrorId -eq "ModuleNotFoundDuringParse") { 
-        # Typical message: "Could not find the module '<ActiveDirectoryDsc, 6.7.0>'."
-        if ($parseErr.Message -match "<(?<module>\w*),\s(?<version>[\d|.]*)>") {
-            $moduleName = $matches['module']
-            $moduleVersion = $matches['version']
-            # Write-Host "Module '$moduleName' version '$moduleVersion' is missing" -ForegroundColor Yellow
-            $missingModules += [PSCustomObject]@{
-                Name = $moduleName
-                Version = $moduleVersion
-            }
+if (-not (Test-Path -PathType Leaf -Path $dscFilePathResolved)) {
+    throw "file '$dscFilePath' not found"
+}
+
+Write-Host "Getting missing module dependencies for DSC file '$dscFilePathResolved'..." -ForegroundColor Cyan
+$parseResult = [Microsoft.WindowsAzure.Commands.Common.Extensions.DSC.Publish.ConfigurationParsingHelper]::ParseConfiguration($dscFilePathResolved)
+
+$missingModules = @()
+foreach ($parseErr in $parseResult.Errors | Where-Object ErrorId -eq "ModuleNotFoundDuringParse") { 
+    # Typical message: "Could not find the module '<ActiveDirectoryDsc, 6.7.0>'."
+    if ($parseErr.Message -match "<(?<module>\w*),\s(?<version>[\d|.]*)>") {
+        $moduleName = $matches['module']
+        $moduleVersion = $matches['version']
+        # Write-Host "Module '$moduleName' version '$moduleVersion' is missing" -ForegroundColor Yellow
+        $missingModules += [PSCustomObject]@{
+            Name = $moduleName
+            Version = $moduleVersion
         }
-     }
-    return $missingModules
+    }
 }
-else {
-    Write-Host "file '$dscFilePath' not found" -ForegroundColor Red
-}
+return $missingModules
--- scripts/New-DscArchive.ps1
+++ scripts/New-DscArchive.ps1
@@ -1,4 +1,4 @@
-#Requires -PSEdition Desktop
+#Requires -PSEdition Desktop #reason: https://github.com/dsccommunity/DnsServerDsc/issues/264
 #Requires -Module Az.Compute
 
 param(
@@ -6,15 +6,15 @@ param(
     [string] $dscFolderPath
 )
 
-if (Test-Path -PathType Container -Path $dscFolderPath) {
-    if ($vmName.StartsWith("dsc-")) { $vmName = $vmName.Substring(4) }
-    Write-Host "Generating DSC archives in folder '$dscFolderPath' for '$vmName'" -ForegroundColor Cyan
-    $dscSourceFilePaths = Get-ChildItem $dscFolderPath -File -Filter "dsc-$vmName*.ps1"
-    foreach ($dscSourceFilePath in $dscSourceFilePaths) {
-        $dscArchiveFilePath = "$($dscSourceFilePath.DirectoryName)\$($dscSourceFilePath.BaseName).zip"
-        Publish-AzVMDscConfiguration -ConfigurationPath "$dscFolderPath\$($dscSourceFilePath.Name)" -OutputArchivePath $dscArchiveFilePath -Force -Verbose
-    }
+if (-not (Test-Path -PathType Container -Path $dscFolderPath)) {
+    throw "folder '$dscFolderPath' not found"
 }
-else {  
-    Write-Host "folder '$dscFolderPath' not found" -ForegroundColor Red
+
+if ($vmName.StartsWith("dsc-")) { $vmName = $vmName.Substring(4) }
+$dscSourceFilePaths = Get-ChildItem $dscFolderPath -File -Filter "dsc-$vmName*.ps1"
+foreach ($dscSourceFilePath in $dscSourceFilePaths) {
+    Write-Host "Creating DSC archive for '$($dscSourceFilePath.BaseName)' in folder '$dscFolderPath'..." -ForegroundColor Cyan
+    $dscArchiveFilePath = "$($dscSourceFilePath.DirectoryName)\$($dscSourceFilePath.BaseName).zip"
+    Publish-AzVMDscConfiguration -ConfigurationPath "$dscFolderPath\$($dscSourceFilePath.Name)" -OutputArchivePath $dscArchiveFilePath -Force #-Verbose
 }
+    
