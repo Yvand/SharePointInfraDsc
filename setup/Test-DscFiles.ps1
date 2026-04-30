b4e40da52bb557c2dad5dc35cf10ca6d170a5ca2
Yvan Duhamel <yvandev@outlook.fr>
2026-04-30 18:47:42 +0200

    Update Test-DscFiles.ps1

--- setup/Test-DscFiles.ps1
+++ setup/Test-DscFiles.ps1
@@ -15,16 +15,11 @@ if ($vmName.StartsWith("dsc-")) { $vmName = $vmName.Substring(4) }
 $dscSourceFilePaths = Get-ChildItem $dscFolderPath -File -Filter "dsc-$vmName*.ps1"
 foreach ($dscSourceFilePath in $dscSourceFilePaths) {
     $testFileName = [string]::Format($testFileNamePattern, $dscSourceFilePath.BaseName)
-    if (-not (Test-Path -PathType Leaf -Path "$($dscSourceFilePath.DirectoryName)\$($testFileName).ps1")) {
+    $testFilePath = "$($dscSourceFilePath.DirectoryName)\$($testFileName).ps1"
+    if (-not (Test-Path -PathType Leaf -Path $testFilePath)) {
         throw "test file '$testFileName.ps1' is missing."
     }
     
     Write-Host "Run test '$testFileName'..." -ForegroundColor Cyan
-    try {
-        . "$($dscSourceFilePath.DirectoryName)\$($testFileName).ps1"
-    }
-    catch {
-        Write-Error "test '$testFileName' failed: $($_.Exception.Message)"
-        return
-    }
+    . $testFilePath    
 }
