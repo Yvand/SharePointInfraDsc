bc3387642947f7d92c82976df6414144c299f285
Yvan Duhamel <yvandev@outlook.fr>
2026-05-01 11:44:47 +0200

    move and improve test scripts

--- setup/Test-DscFiles.ps1
+++ setup/Test-DscFiles.ps1
@@ -5,10 +5,7 @@ param(
 )
 
 $dscFolderPath = Join-Path -Path $PSScriptRoot -ChildPath "../src" | Resolve-Path
-
-if (-not (Test-Path -PathType Container -Path $dscFolderPath)) {
-    throw "folder '$dscFolderPath' not found"
-}
+$testFolderPath = Join-Path -Path $PSScriptRoot -ChildPath "../tests" | Resolve-Path
 
 $testFileNamePattern = "test-{0}"
 if ($vmName.StartsWith("dsc-")) { $vmName = $vmName.Substring(4) }
@@ -16,7 +13,7 @@ if ($vmName.StartsWith("dsc-")) { $vmName = $vmName.Substring(4) }
 $dscSourceFilesPath = Get-ChildItem $dscFolderPath -File -Filter "dsc-$vmName*.ps1"
 foreach ($dscSourceFilePath in $dscSourceFilesPath) {
     $testFileName = [string]::Format($testFileNamePattern, $dscSourceFilePath.BaseName)
-    $testFilePath = Join-Path -Path $dscFolderPath -ChildPath "$testFileName.ps1" -Resolve
+    $testFilePath = Join-Path -Path $testFolderPath -ChildPath "$testFileName.ps1" -Resolve
     Write-Host "Run test '$testFileName'..." -ForegroundColor Cyan
     . $testFilePath
 }
--- tests/test-dsc-dc.ps1
+++ tests/test-dsc-dc.ps1
@@ -1,10 +1,16 @@
 ﻿#Requires -PSEdition Desktop
 
+$dscFolderPath = Join-Path -Path $PSScriptRoot -ChildPath "../src" | Resolve-Path
+$configFileName = [IO.Path]::GetFileNameWithoutExtension($PSCommandPath).Substring(5)
+$configFilePath = Get-ChildItem $dscFolderPath -File -Filter "$configFileName.ps1"  | Resolve-Path
+$outputPath = ".\$configFileName"
+
 $randomChars = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 8 | ForEach-Object { [char] $_ })
 $password = New-Object -TypeName System.Security.SecureString
 $randomChars.ToCharArray() | ForEach-Object { $password.AppendChar($_) }
 $password.MakeReadOnly()
 
+$functionName = "ConfigDc"
 $Admincreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "yvand", $password
 $AdfsSvcCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "adfssvc", $password
 $SqlSvcCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqlsvc", $password
@@ -15,17 +21,12 @@ $SPServerName = "SP"
 $SharePointSitesAuthority = "spsites"
 $SharePointCentralAdminPort = 5000
 
-$configFileName = [IO.Path]::GetFileNameWithoutExtension($PSCommandPath).Substring(5)
-$functionName = "ConfigDc"
-$outputPath = ".\$configFileName"
-Push-Location $PSScriptRoot
 try
 {
-    . ".\$configFileName.ps1"
+    . "$configFilePath"
     & $functionName -Admincreds $Admincreds -AdfsSvcCreds $AdfsSvcCreds -SqlSvcCreds $SqlSvcCreds -SPSetupCreds $SPSetupCreds -DomainFQDN $DomainFQDN -PrivateIP $PrivateIP -SPServerName $SPServerName -SharePointSitesAuthority $SharePointSitesAuthority -SharePointCentralAdminPort $SharePointCentralAdminPort -ConfigurationData @{AllNodes=@(@{ NodeName="localhost"; PSDscAllowPlainTextPassword=$true })} -OutputPath $outputPath
-    Remove-Item -Path $outputPath -Recurse
 }
 finally
 {
-    Pop-Location
+    Remove-Item -Path $outputPath -Recurse
 }
